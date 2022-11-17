6303db8450c30e0af09c03df814b6a0598eb1aef
Johnny <phuongnam.nextx@gmail.com>
2022-11-17 22:29:51 +0700

    first commit

---
+++ src/test/java/com/nextx/K8sGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
